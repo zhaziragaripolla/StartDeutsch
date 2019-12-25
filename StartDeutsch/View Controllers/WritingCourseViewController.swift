bd9dee6d2548c5f01ab7dbc37cdb4e2903b3365f
Zhazira <g.zhazira@gmail.com>
2019-12-25 23:35:16 +0500

    create/ view contollers

---
+++ StartDeutsch/View Controllers/WritingCourseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WritingCourseViewController.swift
+//  StartDeutsch
+//
+//  Created by Zhazira Garipolla on 12/25/19.
+//  Copyright © 2019 Zhazira Garipolla. All rights reserved.
+//
+
+import UIKit
+
+class WritingCourseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
