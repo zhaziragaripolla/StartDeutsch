c71ea7fbb96b097d3c1872af2bbe7f11cf193d4e
Zhazira <g.zhazira@gmail.com>
2019-12-27 14:55:30 +0500

    add table view

--- StartDeutsch/View Controllers/WritingCourseViewController.swift
+++ StartDeutsch/View Controllers/WritingCourseViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class WritingCourseViewController: UIViewController {
 
+    let tableView = UITableView()
+    let writingCourseTitles = ["Teil 1", "Teil 2"]
+    
+    fileprivate func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints({ make in
+            make.edges.equalToSuperview()
+        })
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupTableView()
+        tableView.reloadData()
+    }
 
+}
+
+extension WritingCourseViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return writingCourseTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = writingCourseTitles[indexPath.row]
+        return cell
     }
 
 }
