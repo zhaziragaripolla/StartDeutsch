069c4ed49f767b40db66cc85ec39231418743f90
Zhazira <g.zhazira@gmail.com>
2020-01-15 23:23:41 +0500

    refactor/ switch to FirebaseUI to download images

--- StartDeutsch/Custom Views/Extensions.swift
+++ StartDeutsch/Custom Views/Extensions.swift
@@ -7,16 +7,12 @@
 //
 
 import UIKit
+import FirebaseUI
 
 extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
-            }
-        }
+    func load(from path: String) {
+        let reference = Storage.storage().reference(withPath: path)
+        self.sd_setImage(with: reference)
     }
 }
 
