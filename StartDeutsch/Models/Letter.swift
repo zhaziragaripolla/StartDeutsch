a756b10fcb47ccc42cd7ec7f374ce06332e37aa7
Zhazira <g.zhazira@gmail.com>
2020-05-26 21:26:58 +0500

    delete unnecessary files

--- StartDeutsch/Models/Letter.swift
+++ StartDeutsch/Models/Letter.swift
@@ -25,14 +25,3 @@ public struct Letter {
         ]
     }
 }
-
-extension Letter{
-    init?(dictionary: [String : Any]) {
-        guard let id = dictionary["id"] as? String,
-            let title = dictionary["title"] as? String,
-            let task = dictionary["task"] as? String,
-            let points = dictionary["points"] as? [String],
-            let answerImagePath = dictionary["answerImagePath"] as? String else { return nil}
-        self.init(id:id, title: title, task: task, points: points, answerImagePath: answerImagePath)
-    }
-}
