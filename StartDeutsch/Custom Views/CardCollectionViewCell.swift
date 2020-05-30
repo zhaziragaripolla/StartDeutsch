50b52502944ae0d3a103336abb4ba56a52e304ae
Zhazira <g.zhazira@gmail.com>
2020-05-30 22:03:43 +0500

    reorganize files

---
+++ StartDeutsch/Custom Views/CardCollectionViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  CardCollectionViewCell.swift
+//  StartDeutsch
+//
+//  Created by Zhazira Garipolla on 1/6/20.
+//  Copyright © 2020 Zhazira Garipolla. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .medium)
+        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+    
+    let cardImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(cardImageView)
+        cardImageView.snp.makeConstraints({ make in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+        })
+        
+        addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints({ make in
+            make.center.equalToSuperview()
+        })
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
---
+++ StartDeutsch/Custom Views/CardView.swift
@@ -0,0 +1,85 @@
+//
+//  CardView.swift
+//  StartDeutsch
+//
+//  Created by Zhazira Garipolla on 1/14/20.
+//  Copyright © 2020 Zhazira Garipolla. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    let cardImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let indicatorButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.borderWidth = 2.0
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.layer.backgroundColor = UIColor.white.cgColor
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        let widthConstraint = button.widthAnchor.constraint(equalToConstant: 24.0)
+        let heightConstraint = button.heightAnchor.constraint(equalToConstant: 24.0)
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint])
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        layer.cornerRadius = 20
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2.0
+        addSubview(cardImageView)
+        cardImageView.snp.makeConstraints({ make in
+            make.width.equalToSuperview().multipliedBy(0.85)
+            make.height.equalToSuperview().multipliedBy(0.8)
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+        })
+        
+        addSubview(indicatorButton)
+        indicatorButton.snp.makeConstraints({ make in
+            make.top.equalTo(cardImageView.snp.bottom)
+            make.leading.equalTo(cardImageView.snp.leading)
+            make.bottom.lessThanOrEqualToSuperview().offset(-5)
+        })
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
+
+extension CardView {
+    func setState(_ answerState: AnswerState){
+        switch answerState {
+        case .correct:
+            backgroundColor = Answer.setColor(.correct)
+            indicatorButton.setImage(UIImage(named: "correct"), for: .normal)
+            layer.borderColor = UIColor.white.cgColor
+        case .mistake:
+            backgroundColor = Answer.setColor(.mistake)
+            indicatorButton.setImage(UIImage(named: "incorrect"), for: .normal)
+            self.layer.borderColor = Answer.setColor(.mistake).cgColor
+        case .chosen:
+            backgroundColor = Answer.setColor(.chosen)
+            indicatorButton.setImage(UIImage(systemName: "smallcircle.circle.fill"), for: .normal)
+            layer.borderColor = Answer.setColor(.chosen).cgColor
+        }
+    }
+    
+    func reset(){
+        backgroundColor = .white
+        indicatorButton.setImage(nil, for: .normal)
+        layer.borderColor = UIColor.white.cgColor
+    }
+}
