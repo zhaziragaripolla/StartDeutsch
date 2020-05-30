6564dddcb66f97f35b9ac3c31c6383800b8c7db6
Zhazira <g.zhazira@gmail.com>
2020-05-30 11:22:36 +0500

    remove FirebaseFirestore pods

--- Podfile
+++ Podfile
@@ -6,19 +6,13 @@ target 'StartDeutsch' do
   	use_frameworks!
 
   	# Pods for StartDeutsch
-  	# add the Firebase pod for Google Analytics
     pod 'Firebase/Analytics'
-    # add pods for any other desired Firebase products
-    # https://firebase.google.com/docs/ios/setup#available-pods
-    pod 'Firebase/Core'
-    pod 'Firebase/Firestore'
     pod 'Firebase/Storage'
     pod 'FirebaseUI/Storage'
     pod 'SnapKit', '~> 5.0.0'
     pod 'Firebase/Functions'
     pod 'ReachabilitySwift'
-    # Optionally, include the Swift extensions if you're using Swift.
-    pod 'FirebaseFirestoreSwift'
+
 
   	target 'StartDeutschTests' do
     inherit! :search_paths
--- StartDeutsch/Supporting files/AppDelegate.swift
+++ StartDeutsch/Supporting files/AppDelegate.swift
@@ -9,14 +9,11 @@
 import UIKit
 import CoreData
 import Firebase
-import FirebaseFirestore
 import os
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    let container = AppDependencyContainer()
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
@@ -45,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "GermanCourse")
+        let container = NSPersistentContainer(name: "Course")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
