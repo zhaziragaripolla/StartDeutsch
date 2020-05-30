f26aa6c0ee0d3e08a645157b334a65b372bdfdb9
Zhazira <g.zhazira@gmail.com>
2020-05-30 21:29:02 +0500

    add constants to save messages

---
+++ StartDeutsch/Supporting files/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  StartDeutsch
+//
+//  Created by Zhazira Garipolla on 5/30/20.
+//  Copyright © 2020 Zhazira Garipolla. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    struct FailureMessage{
+        static let noInternetConnection = "Oops! This device appears to be offline 😕, please check the internet connection."
+        static let other = "Oops! Something went wrong😕. Please, try again later."
+    }
+}
